be25d567518807ebb0df8baa8a1ab6b9e1845d20
Jorge Alamo <jorgealamosan@gmail.com>
2023-12-13 18:53:05 +0000

    Class FactorialCommand

--- src/main/java/software/ulpgc/kata5/FactorialCommand.java
+++ src/main/java/software/ulpgc/kata5/FactorialCommand.java
@@ -1,4 +1,67 @@
 package software.ulpgc.kata5;
 
-public class FactorialCommand {
+import java.util.stream.IntStream;
+
+public class FactorialCommand implements Command{
+    @Override
+    public Output execute(Input input) {
+        try {
+            int number = Integer.parseInt(input.get(":number"));
+            return isOutOfBound(number) ? outOfBoundOutput() : outputOf(number);
+        }
+        catch (NumberFormatException e) {
+            return nanOutput();
+        }
+    }
+
+    private Output nanOutput() {
+        return new Output() {
+            @Override
+            public int responseCode() {
+                return 405;
+            }
+
+            @Override
+            public String result() {
+                return "Not A Number";
+            }
+        };
+    }
+
+    private Output outputOf(int number) {
+        return new Output() {
+            @Override
+            public int responseCode() {
+                return 200;
+            }
+
+            @Override
+            public String result() {
+                return String.valueOf(factorialOf(number));
+            }
+        };
+    }
+
+    private int factorialOf(int number) {
+        return IntStream.range(2, number+1)
+                .reduce(1, (a, i) -> a*i);
+    }
+
+    private Output outOfBoundOutput() {
+        return new Output() {
+            @Override
+            public int responseCode() {
+                return 404;
+            }
+
+            @Override
+            public String result() {
+                return "Number out of bounds";
+            }
+        };
+    }
+
+    private boolean isOutOfBound(int number) {
+        return number < 1 || number >= 20;
+    }
 }
