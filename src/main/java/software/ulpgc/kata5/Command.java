97c7c79ab054c49a4b8c419194d263c982ca746b
Jorge Alamo <jorgealamosan@gmail.com>
2023-12-13 18:48:47 +0000

    Interface Command

--- src/main/java/software/ulpgc/kata5/Command.java
+++ src/main/java/software/ulpgc/kata5/Command.java
@@ -1,4 +1,17 @@
 package software.ulpgc.kata5;
 
 public interface Command {
+
+    Output execute(Input input);
+
+    interface Input {
+        String get(String parameter);
+    }
+
+    interface Output {
+        int responseCode();
+
+        String result();
+    }
+
 }
